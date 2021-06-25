7f45e2caf8cc6d9ed0d0f1142f6b84c16753e512
Anirudh <anirudhks001@gmail.com>
2021-06-25 09:42:59 +0530

    upsolved a

---
+++ 727_2/a_up.cpp
@@ -0,0 +1,68 @@
+//Author = Anirudh Srikanth (yeetholmes619) [B20CS006]
+
+#include<bits/stdc++.h>
+using namespace std;
+#define RESET   "\033[0m"
+#define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
+#define ll long long
+#define ld long double
+#define INF 1000000007
+#define fu(i,a,b) for(ll i = a; i<=b;i++)
+#define fd(i,a,b) for(ll i = a; i>=b; i--)
+#define fdd(i,a) for(ll i = a; i>=1; i--)
+#define fuu(i,a)for(ll i = 1; i<=a; i++)
+#define pb push_back
+#define pf push_front
+#define cinarr(a) for(auto &zz:a)cin>>zz
+#define allvec(v) v.begin(), v.end()
+#define vstr vector<string>
+#define vll vector<ll>
+#define vint vector<int>
+#define allarr(a,n) a, a+n
+#define MOD 1000000007
+// precedence order :- double > float > long long int > long int > int > char
+// remember that boolean arrays and variables if uninitialized are given false
+//Check if constraints are correct
+// for single arrays declare them as long long int
+//overflow must be taken care of!! use strings when big
+//	0 < |int| < 1e9
+//	0 < |long int| < 1e12
+//	0 < |long long int| < 1e18
+//when you are working with stacks, remember to take care of stacks of 0 size
+//when you see a problem, and you know how you would tackle it in the real world, but don't know how
+//you would do it in code, go step by step, in each step try to be concious of what you want to do
+//and once you have done so, review through and remember what data structure would be perfect
+//when we pass an array in a function the pointer is passed but when we pass a vector, a copy is passed
+
+using namespace std;
+ll n,x,t;
+void take(){
+        cin>>n>>x>>t;
+}
+
+void solve(){
+        take();
+        assert(x != 0);
+        ll c1 = (t/x);
+        if((n-1-(c1)) <= 0LL) {
+                cout<<(n*n) - ((n*(n+1))/2)<<"\n";
+                return;
+        }
+        ll ans = 0LL;
+        ans += n*c1;
+        ans -= ((c1*c1) + c1)/2;
+        cout<<fixed<<ans<<"\n";
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	ll t = 1;
+	cin >> t;
+	fuu(i, t) {
+		//cout << "Case #" << i << ": ";
+		solve();
+	}
+}
+
