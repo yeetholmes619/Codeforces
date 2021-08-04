25759066e9cc49211a1b915635fa6b0214ad56dd
Anirudh Srikanth <anirudhks001@gmail.com>
2021-08-04 09:06:38 +0530

    made a practice folder for upsolving

---
+++ practice/1485B.cpp
@@ -0,0 +1,107 @@
+//Author = Anirudh Srikanth (yeetholmes619) [B20CS006]
+#include<bits/stdc++.h>
+using namespace std;
+#define RESET   "\033[0m"
+#define BR   "\033[1m\033[31m"      /* Bold Red */
+#define BG   "\033[1m\033[32m"      /* Bold Green */
+#define BB    "\033[1m\033[34m"      /* Bold Blue */
+#define ll long long
+#define ld long double
+#define INF 1000000007
+#define fu(i,a,b) for(ll i = a; i<=b;i++)
+#define fd(i,a,b) for(ll i = a; i>=b; i--)
+#define fdd(i,a) for(ll i = a; i>=1; i--)
+#define fuu(i,a)for(ll i = 1; i<=a; i++)
+#define pb push_back
+#define pf push_front
+#define cinarr(a) for(auto &zz:a)cin>>zz
+#define mp make_pair
+#define allvec(v) v.begin(), v.end()
+#define vstr vector<string>
+#define vll vector<ll>
+#define vint vector<int>
+/**
+ * I/O
+ **/
+using namespace std;
+#define int long long
+#define output(value) cout << value << endl
+#define error(errorString) cout << BR << errorString << RESET<< "\n"
+#define info(infoString) cout << BB << infoString << RESET << "\n"
+#define success(successString) cout << BB << successString << RESET << "\n"
+#define allarr(a,n) a, a+n
+#define MOD 1000000007
+// precedence order :- double > float > long long int > long int > int > char
+// remember that boolean arrays and variables if uninitialized are given false
+//Check if constraints are correct
+// for single arrays declare them as long long int
+//overflow must be taken care of!! use strings when big
+//	0 < |int| < 1e9
+//	0 < |long int| < 1e12
+//	0 < |long long int| < 1e18
+//when you are working with stacks, remember to take care of stacks of 0 size
+//when you see a problem, and you know how you would tackle it in the real world, but don't know how
+//you would do it in code, go step by step, in each step try to be concious of what you want to do
+//and once you have done so, review through and remember what data structure would be perfect
+//when we pass an array in a function the pointer is passed but when we pass a vector, a copy is passed
+void debug(vector<int> v){
+        for(auto t: v) error(t<<" ");
+        cerr<<"\n";
+}
+int n,q,k;
+vector<int> a;
+void take(){
+        cin>>n>>q>>k;
+        a.clear();
+        a.resize(n);
+        cinarr(a);
+}
+
+void solve(){
+        take();
+        vector<int> prefix(n);
+        //edgecase for n == 1
+        prefix[0] = (n == 1 ? k-1 : a[1]-2) ;
+        prefix[n-1] = k - a[n-2] -1;
+        if(n != 1){
+                for(int i = 1; i < n-1; i++){
+                        prefix[i] = a[i+1] - a[i-1] -2;
+                }
+
+                for(int i = 1; i < n; i++){
+                        prefix[i] += prefix[i-1];
+                }
+        }
+
+
+        while(q--){
+                int l,r;
+                cin>>l>>r;
+                if(n == 1){
+                        cout<<k-1<<"\n";
+                        continue;
+                }
+                l--;r--;
+                if(l == r){
+                        cout<<k -1 <<"\n";
+                        continue;
+                }
+                int ans = 0;
+                //edge case for l == r
+                ans += prefix[r-1] - prefix[l];
+                ans += a[l+1] - 2;
+                ans += k - a[r-1] - 1;
+                cout<<ans<<"\n";
+        }
+}
+
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	ll t = 1;
+	fuu(i, t) {
+		//cout << "Case #" << i << ": ";
+		solve();
+	}
+}
